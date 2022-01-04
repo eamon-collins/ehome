2c79247681471378a1bf6e63e98f04f65a5836e9
eamon-collins <eamonacollins@gmail.com>
2022-01-04 03:07:38 -0500

    need to keep up login or else get staleelement when it partially logs out

--- econ/views.py
+++ econ/views.py
@@ -8,6 +8,7 @@ from random import randint
 from .models import Article, Issue
 
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
 
 #relative import to private tool
 import sys
@@ -15,17 +16,37 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'alibabble'))
 import playback as Playback
 
+#whether the system will overwrite the old record of the article
+#or stop and leave the current record. Doesnt really work as true rn
+OVERWRITE = False
 
 
 # Create your views here.
 def index(request):
-	#diagresults = scrape()
+
+
 	scrape("2022-01-01")
+
+	#get_blank_economist_browser()
+
 	return HttpResponse("Under construction")
 
 def scrape_article(browser, issue, article_url):
 	browser.get(article_url)
 	print(article_url)
+
+	dupes = Article.objects.filter(url = article_url)
+
+	if dupes.count() and not OVERWRITE:
+		print("This article url exists in our database, skipping")
+		return None
+	elif dupes.count(): #doesnt really
+		print("This article url exists, re-scraping and overwriting")
+		return dupes.get()
+
+
+
+
 
 	article = Article()
 
@@ -37,10 +58,14 @@ def scrape_article(browser, issue, article_url):
 	#piece together the article text
 	article_string = ""
 	for text_piece in browser.find_elements_by_class_name("article__body-text"):
-		print(text_piece.text)
-		article_string += text_piece.text 
+		#print(text_piece.text)
+		try:
+			article_string += text_piece.text 
+			article_string += "\n"
+		except StaleElementReferenceException as e:
+			print("Stale element exception in article scraping url: " + article_url)
 
-
+	return article
 
 
 
@@ -60,15 +85,26 @@ def scrape(edition_date):
 	try:
 		title = browser.find_element_by_class_name("weekly-edition-header__headline").text
 	except Exception as e:
-		title = "Couldn't find"
-	issue = Issue.objects.create(
-		title = title,
-		date = edition_date
-		)
-	print(title + repr(edition_date))
-	issue.save()
+		title = "Couldn't find issue title"
 
-	#select articles and scrape them
+	#check for pre-existing issue matching this date in our database
+	dupes = Issue.objects.filter(date = edition_date)
+	if dupes.count() == 0:
+		issue = Issue.objects.create(
+			title = title,
+			date = edition_date
+			)
+		print(title + repr(edition_date))
+		issue.save()
+	else: 
+		issue = dupes.get()
+		print("We already have a record of an issue for this date")
+
+
+
+	#Now compose a list of article links in the latest weekly issue
+	#get articles by following links and scrape them
+	#then save them to db.
 	headline_elements = browser.find_elements_by_class_name("headline-link")
 
 	#necessary to get links first so don't get StaleElements
@@ -77,14 +113,14 @@ def scrape(edition_date):
 		article_links.append(headline.get_attribute("href"))
 	article_objects = []
 	for link in article_links:
-		article_objects.append(scrape_article(browser, issue, link))
+		article = scrape_article(browser, issue, link)
+		if article: #makes sure a None was not returned, which denotes a skip.
+			article_objects.append(article)
 		
-
 	Article.objects.bulk_create(article_objects)
 
 
 
-	#Now compose a list of articles in the latest weekly issue
 
 
 
@@ -92,65 +128,25 @@ def login(browser):
 	loginurl = 'https://economist.com'
 	browser.get(loginurl)
 
+	#THESE LOGINS AREN"T TO A PAID ACCOUNT
 	#execute manual one of 3 manual recordings
 	rec_path = "econ/recordings/login" + str(randint(1,3)) +".rec"
+
+	#THIS LOGIN IS TO A PAID ACCOUNT
+	rec_path = "econ/recordings/login-paid.rec"
+
 	#rec_path = "econ/recordings/loginfast.rec"
 	Playback.playback(os.path.abspath(rec_path), 1)
  
 
+#debugging method to simply open up a browser with economist.com
+#helps get a browser in the right position to record login mouse
+#and keyboard movements.
+def get_blank_economist_browser():
+	#start a browser session
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument("--disable-infobars")
+	browser = webdriver.Chrome('/home/eamon/repos/ehome/chromedriver', chrome_options=chrome_options)
 
+	browser.get('https://economist.com')
 
-def OLD_scrape():
-	#if it's saturday
-	if datetime.now().weekday() == 5 or True:
-
-		destinationurl = 'https://www.economist.com/user/login?destination=%2Fna%2Fprintedition%2F2018-05-26' #2018-05-26'
-		#destinationurl += datetime.today().isoformat()
-
-
-		headers = requests.utils.default_headers()
-		headers.update(
-		    {
-		        'User-Agent': 'Mozilla/5.0',
-		    }
-		)
-		
-		s = requests.Session()
-		loginurl = 'https://authenticate.economist.com/login'
-		payload = {'state': '-rwGqfTTxshJUaBJx6ov88bc2Z_5Al-H',
-					'client': 'IARGLj7TVzFnBSYoW94mIzZJAe3U5vaq',
-					'protocol': 'oauth2',
-					'response_type': 'token%20id_token',
-					'redirect_uri': destinationurl,
-					'scope': 'openid%20profile',
-					'nonce': 'ff5bcmjcshrizlmzb8iml',
-					'additional_parameters':'e30%3D',
-					'auth0Client':'eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS42LjEifQ%3D%3D'}
-		login_page = s.get(loginurl, headers=headers, params=payload)
-		login = BeautifulSoup(login_page.text, 'lxml')
-		form = login.find('body')#works attrs={'name': "form_build_id"}
-		print(form)
-		for script in login(["noscript", "style"]):
-			script.decompose()
-		for script in login(["script", "style"]):
-			script.decompose()
-
-		text = login.get_text()
-		return JsonResponse({'result':text})
-
-		# payload = {
-		#             'name' : 'ec3bd@virginia.edu',
-		#             'pass' : 'zizou361',
-		#             'form_build_id' : form['value'],
-		#             'form_id' : 'user_login',
-		#             'securelogin_original_baseurl' : 'https://www.economist.com',
-		#             'op' : 'Log in'
-		#             }
-
-		response = s.post(loginurl, data=payload, headers=headers)
-
-		edition = BeautifulSoup(response.text, 'lxml')
-		#print(edition.find('href'))
-
-
-		return None
